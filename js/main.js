89b0d428be04ac09d326d08e7c22e70b82d0c63e
SIlas <you@example.com>
2020-09-18 17:31:36 +0300

    count project

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// set intial value to zero
+let count=0;
+const value=document.querySelector('#value');
+const btns=document.querySelectorAll('.btn');
+
+btns.forEach(function (btn){
+    btn.addEventListener('click',function(e){
+        const styles=(e.currentTarget.classList);
+        if(styles.contains("decrease"))
+        {
+            count--;
+        }
+        else if(styles.contains("increase"))
+        {
+            count ++;
+        }
+       if(styles.contains("reset"))
+       {
+           count=0;
+       }
+       if(count>0)
+       value.style.color="red";
+       else if(count<0)
+       {
+           value.style.color="white";
+       }
+       if(count==0)
+       {
+          value.style.color="black"
+       }
+
+        value.textContent=count;
+        
+
+    })
+
+    
+
+    });
+
+
+
